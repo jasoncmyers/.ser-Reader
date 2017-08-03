903e125fab2207925a3636f1b9611cdfd1223327
Jason Myers <myers.jason.c@gmail.com>
2017-08-03 12:04:44 -0500

    Backup original converter program and begin modifying to use new class structure

--- SER reader/SER converter.cpp
+++ SER reader/SER converter.cpp
@@ -4,17 +4,21 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 #include "serReader.hpp"
 
 using namespace std;
 
-SerHeader header;
+SerReader header;
 
 
 int _tmain(int argc, char* argv[])
 {
 	fstream serFile;
 	fstream outFile;
+
+	SerReader reader;
 	
 
 	serFile.open("test.ser", ios::in | ios::binary);
@@ -30,7 +34,9 @@ int _tmain(int argc, char* argv[])
 	if(serFile.is_open())
 	{
 		cout << "File opened\n\n";
-		ReadHeaders(&serFile, header);
+		reader.SetFile(&serFile);
+
+		SerReader::SerHeader header = reader.header;
 
 
 	cout << "Header structure:" << endl;
@@ -69,125 +75,8 @@ int _tmain(int argc, char* argv[])
 		cout << "Unit description: " << header.dimHeaders[1].unitName << endl;
 	}
 
-	__int64* dataOffs = NULL;
-	__int64* tagOffs = NULL;
-	DataTag* tags = NULL;
-	//D2DataSet dataSet;
-	//D2DataSet* dataSets = NULL;
-	D2DataSet dataSet;
-	dataSet.data = NULL;
-	D2DataSet* dataSets = NULL;
-	
-	int test;
-
-
-	test = ReadOffsetArrays(&serFile, header, dataOffs, tagOffs);
-	if(test != 0)
-		cout << "Error with ReadOffsetArrays: " << test << endl;
-
-	for(int i = 0; i < header.totNumElem; i++) {
-		cout << "DataOffset " << dec << i << " = " << hex << dataOffs[i] << "; TagOffset " << dec << i << " = " << hex << tagOffs[i] << endl;
-	}
-	
-	test = ReadAllTags(&serFile, header, tagOffs, tags);
-	if(test != 0)
-		cout << "Error with ReadAllTags: " << test << endl;
-
-	test = Read2DDataSet(&serFile, header, dataOffs, dataSet, 0);
-	if (test != 0)
-		cout << "Error with Read2DDataSet: " << test << endl;
-
-	/*
-	cout <<"\n\nDataset values:\n";
-	cout << "calOffset X: " << dec << dataSet.calOffsetX << endl;
-	cout << "calDelta X: " << dec << dataSet.calDeltaX << endl;
-	cout << "calElem X: " << dataSet.calElementX << endl;
-	cout << "data type: " << dataSet.dataType << endl;
-	cout << "Array size: (" << dataSet.arraySizeX << ", " << dataSet.arraySizeY << ")\n";
-	*/
-	
-	/*
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			cout << "Pixel (" << i << ", "<< j<<") intensity = " << dec << (__int16)dataSet.data[i+j*dataSet.arraySizeX].sIntData << endl;
-		}
-	}
-	*/
-
-	
-	/*
-	cout << "\n\n* * * Beginning full data readout * * *\n";
-	test = ReadAll2DDataSets(&serFile, header, dataOffs, dataSets);
-	if (test != 0)
-		cout << "Error with ReadAll2DDataSets: " << test << endl;
-	cout << "\n\n* * * Full data readout complete * * *\n\n";
-	
-
-	for(int i = 0; i < header.totNumElem; i++)
-	{
-		cout << "Dataset " << dec << i << " has weird seperator tag 0x" << hex << tags[i].weirdFinalTags << endl;
-	}
-	*/
-
-	
-
-	for(int i = 0; i < header.validNumElem; i++)
-	{
-		Read2DDataSet(&serFile, header, dataOffs, dataSet, i);
-
-
-	
-	// Test wherein we actually modify the data to remove the clipped pixel values!!
-	cout << "\n\n* * * Beginning modifying pixel intensities of dataset " << dec << i << " * * *\n";
-	
-		int numPoints = dataSet.arraySizeX * dataSet.arraySizeY;
-		for(int j = 0; j < numPoints; j++)
-		{
-			int tempVal = (__int16)dataSet.data[j].sIntData;
-			if(tempVal < -1000) 
-			{
-				//cout << "Member " << dec << j << " of dataset " << i << " is clipped.  Reseting intensity to max.\n";
-				dataSet.data[j].sIntData = 25000;
-			}
-		}
-	
-
-		int eCode = Overwrite2DData(&outFile, header, dataOffs, dataSet, i);
-		if(eCode == 0)
-			cout << "Overwrite on dataset " << i << " complete\n";
-		else
-			cout << "Overwrite failed on dataset " << i << ": error code " << eCode << "\n";
-	}
-
-
-	// Test writing the data to a new file
-	/*
-	cout << "\n\n* * * Beginning output of test file * * *\n";
-	outFile.open("testout.ser", ios::out | ios::binary);
-
-	test = WriteHeaders(&outFile, header);
-	if (test != 0)
-		cout << "Error with WriteHeaders: " << test << endl;
-	test = WriteOffsetArray(&outFile, header, dataOffs, tagOffs);
-	if (test != 0)
-		cout << "Error with WriteOffsetArray: " << test << endl;
-	test = WriteAll2DDataAndTags(&outFile, header, dataSets, tags);
-	if (test != 0)
-		cout << "Error with WriteAll2DDataAndTags: " << test << endl;
-	cout << "* * * Test file output complete * * *\n\n";
-	*/
-
 	serFile.close();
 	outFile.close();
-
-
-
-	delete[] header.dimHeaders;
-	delete[] dataOffs;
-	delete[] tagOffs;
-	delete[] tags;
 		
 	}
 	
